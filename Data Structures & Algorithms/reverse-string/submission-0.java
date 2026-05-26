29537bf17dcc9dbfb39a1504cf86fe261c92fcb5
Kishore_Ram <166026696+KishoreXtobi@users.noreply.github.com>
2026-05-26 09:22:19 +0530

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int lptr = 0, rptr = s.length - 1;
+        while(lptr<rptr){
+            char temp = s[lptr];
+            s[lptr] = s[rptr];
+            s[rptr]=temp;
+            lptr++;
+            rptr--;
+        }
+    }
+}
